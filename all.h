6c9e5b40b1e2e16e4322d07b526a919cdb83fd7e
mame0622 <mame.like.ww@gmail.com>
2023-01-12 11:19:56 +0900

    no message

--- all.h
+++ all.h
@@ -1,6 +1,5 @@
 #pragma once
 <<<<<<< HEAD
-<<<<<<< HEAD
 //******************************************************************************
 //
 //
@@ -10,25 +9,14 @@
 //******************************************************************************
 
 //システムやライブラリのヘッダーファイルは上の方へ
-=======
-
->>>>>>> 23c190ec252f7c8ce4c78606a1c372ff6cc960f8
 #include "../GameLib/game_lib.h"
 #include "../GameLib/input_manager.h"
 #include "../GameLib/obj2d_data.h"
 #include "../GameLib/depth_stencil.h"
 
-<<<<<<< HEAD
-#include "common.h"
-=======
->>>>>>> 23c190ec252f7c8ce4c78606a1c372ff6cc960f8
 
 #include "Scene.h"
 #include "SceneTitle.h"
 #include "SceneGame.h"
-<<<<<<< HEAD
 
 using namespace GameLib;
-=======
-#include "SceneClear.h"
->>>>>>> 23c190ec252f7c8ce4c78606a1c372ff6cc960f8
