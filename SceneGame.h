23c190ec252f7c8ce4c78606a1c372ff6cc960f8
いちごミルク2\2220287 <619345haruto@gmail.com>
2023-01-12 11:16:48 +0900

    no message

--- SceneGame.h
+++ SceneGame.h
@@ -1,1 +1,46 @@
 #pragma once
+
+//******************************************************************************
+//
+//
+//      ゲームシーン
+//
+//
+//******************************************************************************
+
+//==============================================================================
+//
+//      Gameクラス
+//
+//==============================================================================
+
+class Game : public Scene
+{
+private:
+    bool            isPaused_;
+    static Game     instance_;
+    bool            gameOver_;
+    int             overTimer_;
+
+public:
+    static Game* instance() { return &instance_; }
+
+
+    void init() override;
+    void deinit() override;
+    void update() override;
+    void draw() override;
+
+    bool getGameOver()const { return this->gameOver_; }
+private:
+    Game()
+        : isPaused_(false)
+        , gameOver_(false)
+        , overTimer_(0)
+    {}
+    Game(const Game&) = delete; // = delete コピーコンストラクタが存在しないことを明示
+
+};
+
+//******************************************************************************
+
